a9f312fd6f5e7a001255e767882f69684a477e4b
mg <mg@mg>
2025-10-21 18:18:22 +0200

    New version of AJTC in React
    Put this file ajtc_react.sh into properly configuret React/Jest project. First argument is a zip with a SubmittedFiles folder from students work. It will produce the output folder with a final.mg_log in which points will be stored

--- ajtc_react.sh
+++ ajtc_react.sh
@@ -2,6 +2,8 @@
 #First argument is a zip folder with a students work
 #second argument is a fodler with tests
 #You have to install dtrx in python venv
+#this file should be inside a root of proper configured react project
+#zip itself should only contain a folder named "Submitted files"
 
 if ! [ -x "$(command -v dtrx)" ]; then
   echo 'Error: dtrx is not installed.' >&2
@@ -29,62 +31,56 @@ popd () {
 }
 
 ARCHIVE_NAME=$1
-TESTS_PATH=$(realpath $2)
-TESTS_PATH="$(echo $TESTS_PATH| tr -d '\r')"
-echo "TESTS_PATH"
-echo $TESTS_PATH
 pushd .
-cd $TESTS_PATH
-cd ..
-rm -rf src/*
 npm i
-popd
 
 FOLDER_NAME="${ARCHIVE_NAME%.*}"
-rm -rf $FOLDER_NAME
+rm -rf "${FOLDER_NAME}"
+echo "Unpacking"
 dtrx -r -n $ARCHIVE_NAME
 #remove the extension, as this will be name of folder from dtrx
+echo "Done unpacking"
 
-cd $FOLDER_NAME
+cd "${FOLDER_NAME}"
 
 #remove all archives
+find . -type d -empty -delete
 fdfind -Hitf '\.(zip|tar|tar\.gz|tgz|tar\.bz2|tbz2|tar\.xz|txz|7z|rar|iso|gz|bz2|xz|lzma|zst|cab|ar|deb|rpm)$' -X rm -rf
 fdfind  -t d -Hi node_modules -X rm -rf
-#find a base folder.
+popd
 
-# Use fd to find files and save trimmed paths into an array
+pwd=$PWD
+components_pwd="${pwd}/src/"
+output_dir="${pwd}/outputs/"
+rm -rf "${output_dir}"
+mkdir -p "${output_dir}"
+echo "remove ${components_pwd}"
+rm -rf "${components_pwd}"
+mkdir -p "${components_pwd}"
+
+#Find all folders that contains Counter.jsx, its a base root for src/components
+#copy all files from this folder to local one and run tests
 mapfile -t trimmed_paths < <(fdfind  -t f -Hi '^Counter.jsx$' -x bash -c '
     trimmed="${1%/*}"
     echo "$trimmed"
 ' bash {})
 
 for trimmed in "${trimmed_paths[@]}"; do
-    pushd .
-    GO_INTO="$(echo $trimmed| tr -d '\r')"
-    echo "GO INTO"
-    echo $GO_INTO
-    cd "$GO_INTO"
-    TEMP_PWD="$(echo $PWD| tr -d '\r')"
+echo "copy from ${trimmed}"
+rm -rf "${components_pwd}"
+mkdir -p "${components_pwd}"
 
-
-    echo "Copy from ${TEMP_PWD}"
-    echo "Copy to  ${TESTS_PATH}"
-    rm -rf ${TESTS_PATH}/components
-    cp -r "${TEMP_PWD}" "${TESTS_PATH}" 
-    cd "${TESTS_PATH}"
-
-    echo "NAME"
-    NAME="$(echo ${GO_INTO} | awk -F '/' '{printf("%s" , $3)}' | tr -s ' ' '_')"
-    echo $NAME
-    anothervariable="$NAME".mg_log
-
-    npm test run &> $anothervariable
-    echo "NEXT"
-    popd 
-
+cp -r "${trimmed}" ./src/
+GO_INTO="$(echo $trimmed| tr -d '\r')"
+#get a name of student
+NAME="$(echo ${GO_INTO} | awk -F '/' '{printf("%s" , $4_$5_$6)}' | tr -s ' ' '_')"
+echo "RUN FOR : ${NAME}"
+#save output of npm run to log_file
+log_file="${output_dir}/${NAME}.mg_log"
+npm run test ./tests &> "${log_file}"
 done
-cd $TESTS_PATH
-rg -P '^\s*Tests' -g '*.{mg_log}' | awk -F '/' '{printf("%-35s %-35s\n" , $2, $NF)}' | sed 's/wynik.mg_log://' &> final.mg_log
+#go into output and look for all mg_log. Grep over them to get nice table with tests
+cd "${output_dir}"
+rg -P '^\s*Tests' -g '*.{mg_log}' | awk -F '/' '{printf("%s %-35s\n" , $2, $NF)}' | sed 's/.mg_log:Tests://' | column -t &> final.mg_log
 cat final.mg_log | sort
 echo "DONE"
-
